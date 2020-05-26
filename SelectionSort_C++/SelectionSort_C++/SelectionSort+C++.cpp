703ea009ed113adae3de2fbda60f645a216768ad
현청 박 <qkrguscjd2@naver.com>
2020-05-26 16:22:10 +0900

    Selection_sort C++

---
+++ SelectionSort_C++/SelectionSort_C++/SelectionSort+C++.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int min, index, temp;
+	int array[10] = { 1,5,7,8,2,9,10,3,6,4 };
+	
+	for (int i = 0; i < 10; i++) {
+		min = 9999;
+		for (int j = i; j < 10; j++) {
+			if (min > array[j]) {
+				min = array[j];
+				index = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[index];
+		array[index] = temp;
+	}
+
+	for (int i = 0; i < sizeof(array) / sizeof(int);i++) {
+		printf("%d ", array[i]);
+	}
+}
