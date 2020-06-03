dfb46384ed32853624fb44d866b65f57b6045a84
현청 박 <qkrguscjd2@naver.com>
2020-06-03 11:15:17 +0900

    queue

---
+++ Queue/Queue/Queue.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+	queue<int> q;
+	q.push(4);
+	q.push(3);
+	q.push(2);
+	q.push(1);
+	q.pop();
+	q.pop();
+	while (!q.empty()) {
+		cout << q.front() << ' ';
+		q.pop();
+	}
+}
+
