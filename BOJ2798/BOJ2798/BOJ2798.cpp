8b587b259a88862f490e28ea103e0d83a09489d0
현청 박 <qkrguscjd2@naver.com>
2020-06-16 09:32:18 +0900

    blackjack

--- BOJ2798/BOJ2798/BOJ2798.cpp
+++ BOJ2798/BOJ2798/BOJ2798.cpp
@@ -8,7 +8,9 @@ vector<int> v;
 vector<int> result;
 
 int MaxSum(int M) {
-	int max = 0, sum;
+	int max = 0, sum;//최대 값 , 세수의 합 변수
+	
+	//반복문을 통해 세수의 합 구하기
 	for (int i = 0; i < v.size() - 2; i++) {
 		if (v[i] > M) break;
 		for (int j = i + 1;j < v.size() - 1;j++) {
@@ -18,6 +20,7 @@ int MaxSum(int M) {
 				//	printf("%d %d %d = %d\n", v[i], v[j], v[k], sum);
 				if (sum > M) break;
 				else if ((M - sum) < (M - max)) max = sum;
+				//구한 최대값의 차가 그전 최대값의 차가 작으면 대입
 			}
 		}
 	}
@@ -32,7 +35,7 @@ int main()
 		scanf("%d", &temp);
 		v.push_back(temp);
 	}
-	sort(v.begin(), v.end()); //오름차순 정렬
+	sort(v.begin(), v.end()); //빠른 반복문을 위한 오름차순 정렬
 
 	printf("%d", MaxSum(max));
 }
