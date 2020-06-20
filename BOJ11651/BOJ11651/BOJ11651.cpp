d5a2177c65088501513a23e4b1a476069e08bb98
HyunChungPark <qkrguscjd2@naver.com>
2020-06-20 20:56:59 +0900

    BOJ11651

---
+++ BOJ11651/BOJ11651/BOJ11651.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int> b) {
+	if (a.second < b.second) return true;
+	else if (a.second == b.second) return true;
+	return false;
+}
+int main()
+{
+	int N;
+	scanf("%d", &N);;
+	vector<pair<int, int> > v(N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &v[i].first, &v[i].second);
+	}
+
+	sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < N; i++) {
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+	return 0;
+}
