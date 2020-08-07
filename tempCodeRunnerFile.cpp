4ac95212d0af28331f6c7c0e68afa77124967cb9
HyunChungPark <qkrguscjd2@naver.com>
2020-08-07 15:15:42 +0900

    tree

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,1 @@
 
-    for(int i = 1; i < N; i++){
-        if(cfn[i].first >= result[j].second){
-            result.push_back(cfn[i]);
-            j++;
-            cout<<cfn[i].first<<","<<cfn[i].second<<" ";
-        } 
-    }
-    cout<<j+1;
-}
