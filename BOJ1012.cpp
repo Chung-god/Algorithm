6eb5811116301c3ee5748e2d0b3500fce183c956
HyunChungPark <qkrguscjd2@naver.com>
2020-08-12 15:08:33 +0900

    유기농배추

--- BOJ1012.cpp
+++ BOJ1012.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 using namespace std;
 
+int test;
 int M;
 int N;
 int K; // 배추 개수
+int bug = 0;
 bool farm[51][51];
-bool visited[51][51];
-
-void dfs(int x){
-    if(visited) return;
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+bool visited[4];
+void dfs(int x,int y){
+    farm[x][y] = 0;
+    for(int i = 0 ; i < 4; i++){
+        int Nx = x + dx[i];
+        int Ny = y + dy[i];
+        if(Nx < 0 || Nx > M || Ny < 0 || Ny > N)  continue;
+        if(!farm[Nx][Ny]) continue;
+        dfs(Nx,Ny);
+    }
+    return;
 }
 
 int main(){
-    cin>>M>>N>>K;
-    for(int i = 0 ; i < K; i++){
-        int t1,t2;
-        cin>>t1>>t2;
-        farm[t1-1][t2-1] = 1;
-    }
+    cin>>test;
+    while(test > 0){
+        cin>>M>>N>>K;
+        for(int i = 0 ; i < K; i++){
+            int t1,t2;
+            cin>>t1>>t2;
+            farm[t1][t2] = 1;
+        }
 
-    for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++)
-            cout<<farm[i][j]<<" ";
-        cout<<"\n";   
-    }
+        // for(int i = 0; i < M; i++){
+        //     for(int j = 0 ; j < N; j ++)
+        //         cout<<farm[i][j]<<" ";
+        //     cout<<"\n";
+        // }
 
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(farm[i][j]){
+                    dfs(i,j);
+                    bug++;
+                } 
+            }
+        }
+
+        cout<<bug<<"\n";
+        bug = 0;
+        test--;
+    }
 }
