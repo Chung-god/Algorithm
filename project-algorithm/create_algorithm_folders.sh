7d0ad04db078e12a2dec8ca4ba600179883714d1
Chung-god <qkrguscjd254@gmail.com>
2025-01-05 23:12:30 +0900

    2025 folder made

---
+++ project-algorithm/create_algorithm_folders.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir Greedy \
+      DynamicProgramming \
+      DivideAndConquer \
+      Backtracking \
+      Graph \
+      Tree \
+      BinarySearch \
+      Sorting \
+      String \
+      BruteForce \
+      Heap
+
+echo "Algorithm folders created successfully!"
